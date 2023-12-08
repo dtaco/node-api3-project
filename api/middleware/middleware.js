a5437d9f5bc468ebf20743e11e976e917576b1a3
David Tacoronte <d.tacoronte89@gmail.com>
2023-12-08 15:58:07 -0500

    endpoints with id parameter -middleware added

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,5 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
   const timestamp = new Date().toLocaleString()
@@ -7,10 +9,24 @@ function logger(req, res, next) {
   next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
-  console.log('validateUserId middleware')
-  next();
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if (!user) {
+      res.status(404).json({
+        message: 'user not found'
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: `Error finiding user ${req.params.id}`, 
+      err: err.message,
+      stack: err.stack,
+    })
+  }
 }
 
 function validateUser(req, res, next) {
