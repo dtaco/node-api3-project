02f594f00a45660855e1cbfc58959f97548ed805
David Tacoronte <d.tacoronte89@gmail.com>
2023-12-08 16:25:20 -0500

    router has been set up

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {
   validateUserId,
   validateUser,
+  validatePost,
 } = require('../middleware/middleware');
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
@@ -39,12 +40,15 @@ router.delete('/:id', validateUserId, (req, res) => {
 router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  console.log(req.user)
+  console.log(req.text)
 });
 
 // do not forget to export the router
